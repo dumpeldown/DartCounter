2ab7cd5d133707f655ac46ecb7da0b8cb2e5fc85
jjesse <jurek_miklas.jesse@smail.th-koeln.de>
2021-03-07 15:26:48 +0100

    Starter as EntryPoint for application

---
+++ src/main/java/de/dumpeldown/dartcounter/starter/Starter.java
@@ -0,0 +1,10 @@
+package de.dumpeldown.dartcounter.starter;
+
+import de.dumpeldown.dartcounter.gui.DartGui;
+
+public class Starter {
+    public static void main(String[] args) {
+        DartGui dartGui = new DartGui();
+        dartGui.start();
+    }
+}
